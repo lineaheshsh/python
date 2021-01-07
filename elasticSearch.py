aaaf20ecea5abd2c2c6decf09ceed9911ec580c0
LeeChangHo <lineaheshsh@naver.com>
2021-01-07 22:55:40 +0900

    엘라스틱서치 형태소 분석 후 키워드 리스트 추가

--- elasticSearch.py
+++ elasticSearch.py
@@ -2,6 +2,7 @@ import pprint
 import json
 import pymysql.cursors
 import multiprocessing
+import time
 from elasticsearch import Elasticsearch
 
 def selectData():
@@ -10,7 +11,7 @@ def selectData():
     conn = pymysql.connect( host='localhost', user='root', password='1q2w3e4r5t', db='zzangho', charset='utf8', cursorclass=pymysql.cursors.DictCursor )
     curs = conn.cursor()
 
-    sSql = "select contents_id, contents from view_news limit 0, 20"
+    sSql = "select contents_id, contents from view_news"
     curs.execute(sSql)
     rows = curs.fetchall()
 
@@ -24,42 +25,10 @@ def analysis(data):
     #print(es.info())
 
     body= {
-            'tokenizer': {
-                'type': 'nori_tokenizer',
-                'decompound_mode': 'mixed',
-                'stoptags': [
-                            "E",
-                            "IC",
-                            "J",
-                            "MAG",
-                            "MM",
-                            "NA",
-                            "NR",
-                            "SC",
-                            "SE",
-                            "SF",
-                            "SH",
-                            "SL",
-                            "SN",
-                            "SP",
-                            "SSC",
-                            "SSO",
-                            "SY",
-                            "UNA",
-                            "UNKNOWN",
-                            "VA",
-                            "VCN",
-                            "VCP",
-                            "VSV",
-                            "VV",
-                            "VX",
-                            "XPN",
-                            "XR",
-                            "XSA",
-                            "XSN",
-                            "XSV"
-                            ]
-            }, 
+            'tokenizer': 'nori_tokenizer',
+            'filter': [
+                'nori_part_of_speech_basic'
+            ], 
             'text': data['CONTENTS']
     }
 
@@ -80,11 +49,13 @@ def analysis(data):
 
 def update(es, id, keywords):
     body = {
-        'keywords': keywords
+        "doc" : {
+            "keywords" : keywords
+        }
     }
 
-    print('id :: ' + str(id))
-    res = es.index(index='nori_naver_news', id=id, body=body)
+    #print('id :: ' + str(id))
+    res = es.update(index='nori_naver_news', id=id, body=body, doc_type="_doc")
 
     return res
 
@@ -92,7 +63,13 @@ newsData = selectData()
 f = open('test.txt', mode='wt', encoding='utf-8')
 
 if __name__ == '__main__':
-    pool = multiprocessing.Pool(processes=2)
+    secs = time.time()
+    sTime = time.ctime(secs)
+    print('start :: ' + sTime)
+    pool = multiprocessing.Pool(processes=4)
     pool.map(analysis, newsData)
     pool.close()
     pool.join()
+    eecs = time.time()
+    eTime = time.ctime(eecs)
+    print('end :: ' + eTime)
